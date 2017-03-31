228a5c93a8d6afb7abf133d9e3b1d15578f82eea
JustinHydroxide <justinoh97@gmail.com>
2017-03-31 00:30:11 -0700

    Added getFriends() and log()

--- src/core/TwitterImageSync.java
+++ src/core/TwitterImageSync.java
@@ -10,11 +10,13 @@ import java.net.URL;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
 
 import twitter4j.ExtendedMediaEntity;
 import twitter4j.ExtendedMediaEntity.Variant;
+import twitter4j.IDs;
 import twitter4j.MediaEntity;
 import twitter4j.Paging;
 import twitter4j.Status;
@@ -26,6 +28,7 @@ import twitter4j.User;
 public class TwitterImageSync {
 
 	// todo: userid vs. username + folder names
+	// todo http://twitter4j.org/javadoc/twitter4j/RateLimitStatus.html
 
 	Twitter twitter;
 	User user;
@@ -41,6 +44,27 @@ public class TwitterImageSync {
 			System.out.println(te.getMessage());
 			System.exit(-1);
 		}
+	}
+
+	private List<Long> getFriends() {
+		List<Long> friends = new ArrayList<Long>();
+
+		long cursor = -1;
+
+		try {
+			while (cursor != 0) {
+				IDs ids = twitter.getFriendsIDs(cursor);
+
+				for (long l : ids.getIDs()) {
+					friends.add(l);
+				}
+
+				cursor = ids.getNextCursor();
+			}
+		} catch (TwitterException te) {
+		}
+
+		return friends;
 	}
 
 	private List<Status> getStatuses(String username, long since_id, long max_id) {
@@ -66,7 +90,7 @@ public class TwitterImageSync {
 				statuses.addAll(statusPage);
 
 				paging.setMaxId(statuses.get(statuses.size() - 1).getId() - 1);
-				
+
 				System.out.println("Last tweet ID: " + Long.toString(paging.getMaxId()));
 			} catch (TwitterException e) {
 				e.printStackTrace();
@@ -77,33 +101,27 @@ public class TwitterImageSync {
 		return statuses;
 	}
 
-	/*private long initFollowing() {
-	  long since_id = Long.MAX_VALUE;
-	  
-	  List<List<Status>> statuses = new ArrayList<List<Status>>();
-	  
-	  // todo: check user on filter, user already done, etc.
-	  for (following users) {
-	    List<Status> statuses = getStatuses(username, -1, -1);
-	    
-	    // mediaDownload(statuses);
-	    
-	    // problem: last tweet by following is 2013, 2017 -> hometimeline checks back to 2013
-	    // solution: since_id should be based on time(?) init on user began, not "most recent tweet by user"
-	  }
-	  
-	  for (List<Status> list : statuses) {
-	  	if (list.size() > 0) {
-	      since_id = Math.min(since_id, list.get(0).getId());
-	    }
-	  }
-	  
-	  if (since_id == Long.MAX_VALUE) {
-	    since_id = 0;
-	  }
-	  
-	  return since_id;
-	}*/
+	/*
+	 * private long initFollowing() { long since_id = Long.MAX_VALUE;
+	 * 
+	 * List<List<Status>> statuses = new ArrayList<List<Status>>();
+	 * 
+	 * // todo: check user on filter, user already done, etc. for (following
+	 * users) { List<Status> statuses = getStatuses(username, -1, -1);
+	 * 
+	 * // mediaDownload(statuses);
+	 * 
+	 * // problem: last tweet by following is 2013, 2017 -> hometimeline checks
+	 * back to 2013 // solution: since_id should be based on time(?) init on
+	 * user began, not "most recent tweet by user" }
+	 * 
+	 * for (List<Status> list : statuses) { if (list.size() > 0) { since_id =
+	 * Math.min(since_id, list.get(0).getId()); } }
+	 * 
+	 * if (since_id == Long.MAX_VALUE) { since_id = 0; }
+	 * 
+	 * return since_id; }
+	 */
 
 	private void getMedia(List<Status> statuses) {
 		for (Status status : statuses) {
@@ -132,6 +150,7 @@ public class TwitterImageSync {
 					}
 
 					// format path
+					// todo: if (retweet) { create subdirectory for user }
 					String username = status.getUser().getScreenName();
 
 					DateFormat df = new SimpleDateFormat("yyMMdd-HHmmss");
@@ -175,30 +194,51 @@ public class TwitterImageSync {
 		}
 	}
 
-	//long since_id = initFollowing();
+	/*private boolean log() {
+		FileOutputStream out = null;
 
-	//List<Status> newStatuses = getStatuses("", since_id, -1);
+		try {
+			out = new FileOutputStream("output.txt");
+
+			out.write(scanTime);
+			out.write(newestImageTime);
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			if (out != null) {
+				out.close();
+			}
+		}
+	}*/
+
+	// long since_id = initFollowing();
+
+	// List<Status> newStatuses = getStatuses("", since_id, -1);
 
 	// todo retweet dupe
 
 	public static void main(String[] args) {
 		TwitterImageSync tis = new TwitterImageSync();
-		
-		/*boolean init = false;
-		
-		if (!init) {
-			tis.initFollowing();
-		}*/
-		
-		
+
+		/*
+		 * boolean init = false;
+		 * 
+		 * if (!init) { tis.initFollowing(); }
+		 */
+
+		// startup -> getFriends() compare to txt filenames
+		// if (!file.exists()) { getStatuses(); }
+		// else { getStatuses(); since_id = Math.max(scanTime - 1 day,
+		// newestImageTime); }
 
 		List<Status> statuses = tis.getStatuses(args[0], -1, -1);
 
 		tis.getMedia(statuses);
 
-		/*for (Status status : statuses) {
-			System.out.println("@" + status.getUser().getScreenName() + " - " + status.getText());
-		}*/
+		/*
+		 * for (Status status : statuses) { System.out.println("@" +
+		 * status.getUser().getScreenName() + " - " + status.getText()); }
+		 */
 
 	}
 
