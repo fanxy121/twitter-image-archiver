0b658eeb8dabc7fa91365e67440d92d634d15aac
JustinHydroxide <justinoh97@gmail.com>
2017-04-03 23:21:44 -0700

    Sync by getting individual user timelines only now

--- src/core/TwitterImageSync.java
+++ src/core/TwitterImageSync.java
@@ -1,24 +1,27 @@
 package core;
 
 import java.io.BufferedInputStream;
+import java.io.BufferedWriter;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.PrintWriter;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Date;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import twitter4j.ExtendedMediaEntity;
 import twitter4j.ExtendedMediaEntity.Variant;
-import twitter4j.IDs;
-import twitter4j.MediaEntity;
+import twitter4j.PagableResponseList;
 import twitter4j.Paging;
 import twitter4j.Status;
 import twitter4j.Twitter;
@@ -28,8 +31,9 @@ import twitter4j.User;
 
 public class TwitterImageSync {
 
-	// todo: userid vs. username + folder names
-	// todo http://twitter4j.org/javadoc/twitter4j/RateLimitStatus.html
+	// TODO userid vs. username + folder names
+	// TODO singleton + other design patterns
+	// TODO http://twitter4j.org/javadoc/twitter4j/RateLimitStatus.html
 
 	Twitter twitter;
 	User user;
@@ -39,6 +43,11 @@ public class TwitterImageSync {
 	int apiTryCount = 0;
 	static final int MAX_API_TRIES = 3;
 	TwitterException twitterException;
+
+	static final String SYNC_FILE = "sync.txt";
+
+	static final int USER_TIMELINE = 0;
+	static final int FAVORITES = 1;
 
 	public TwitterImageSync() {
 		// verify this doesn't throw exceptions
@@ -72,56 +81,60 @@ public class TwitterImageSync {
 			int secondsUntilReset = te.getRateLimitStatus().getSecondsUntilReset();
 			// wait secondsUntilReset;
 		} else if (te.isCausedByNetworkIssue()) {
-			// todo: pause until network is restored
+			// TODO pause until network is restored
 		} else {
 			throw te;
 		}
 	}
 
-	private List<Long> getFriends() throws TwitterException {
-		List<Long> friends = new ArrayList<Long>();
+	private List<User> getFriends(String username) throws TwitterException {
+		List<User> friends = new ArrayList<User>();
 
 		long cursor = -1;
-		IDs ids;
+		PagableResponseList<User> users;
 
 		while (cursor != 0) {
-			// todo: lambda
+			// TODO lambda
 			while (true) {
 				try {
-					ids = twitter.getFriendsIDs(friendsCursor);
+					users = twitter.getFriendsList(username, cursor, 200);
 					break;
 				} catch (TwitterException te) {
 					handleTwitterException(te);
 				}
 			}
 
-			for (long l : ids.getIDs()) {
-				friends.add(l);
-			}
+			friends.addAll(users);
 
-			cursor = ids.getNextCursor();
+			cursor = users.getNextCursor();
 		}
 
 		return friends;
 	}
 
-	private List<Status> getStatuses(String username, long since_id, long max_id) throws TwitterException {
+	private List<Status> getStatuses(String username, long sinceId, int query) throws TwitterException {
 		List<Status> statuses = new ArrayList<Status>();
 
 		Paging paging = new Paging(1, 200);
 
-		if (since_id >= 0) {
-			paging.setSinceId(since_id);
+		if (sinceId >= 0) {
+			paging.setSinceId(sinceId);
 		}
-		paging.setMaxId((max_id >= 0) ? max_id : Long.MAX_VALUE);
 
 		while (true) {
-			List<Status> statusPage;
+			List<Status> statusPage = new ArrayList<Status>();
 
 			while (true) {
 				try {
-					statusPage = (username != null) ? twitter.getUserTimeline(username, paging)
-							: twitter.getHomeTimeline(paging);
+					switch (query) {
+					case USER_TIMELINE:
+						statusPage = twitter.getUserTimeline(username, paging);
+						break;
+					case FAVORITES:
+						statusPage = twitter.getFavorites(username, paging);
+						break;
+					default:
+					}
 					break;
 				} catch (TwitterException te) {
 					handleTwitterException(te);
@@ -139,45 +152,50 @@ public class TwitterImageSync {
 			// System.out.println("Last tweet ID: " +
 			// Long.toString(paging.getMaxId()));
 		}
-		
+
 		return statuses;
 	}
 
-	private long initSync() throws TwitterException {
-		List<Long> friends = getFriends();
+	// TODO modularize twitterexception handling inside method
+	private boolean sync() throws TwitterException {
+        // user timelines
+        List<User> users = getUsers();
+       
+        for (User user: users) {
+            String username = user.getName();
+ 
+            long sinceIdUser = getSinceId(username);
+ 
+            List<Status> userStatuses = getStatuses(username, sinceIdUser, USER_TIMELINE);
+ 
+            if (userStatuses.size() > 0) {
+                // TODO while loop
+                try {
+                    syncMedia(userStatuses);
+ 
+                    setSinceId(username, userStatuses.get(0).getId());
+                }
+            }
+        }
+ 
+        // favorites
+        long sinceIdFavorites = getSinceIdFavorites();
+ 
+        List<Status> favorites = getStatuses(username, sinceIdFavorites, FAVORITES);
+ 
+        if (favorites.size() > 0) {
+            // TODO while loop
+            try {
+                syncMedia(userStatuses);
+ 
+                setSinceId(username, userStatuses.get(0).getId());
+            }
+        }
+    }
 
-		List<List<Status>> allStatuses;
+	private void syncMedia(List<Status> statuses) {
+		Set<String> usernames = new HashSet<String>();
 
-		long since_id = Long.MAX_VALUE;
-
-		// todo: check user on filter, user already done, etc.
-		for (long user_id : friends) {
-			List<Status> userStatuses = getStatuses(user_id, -1, -1);
-
-			allStatuses.add(userStatuses);
-
-			getMedia(userStatuses);
-		}
-
-		// problem: last tweet by following is 2013, 2017 -> hometimeline checks
-		// back to 2013
-		// solution: since_id should be based on time(?) init on user began, not
-		// "most recent tweet by user" }
-		for (List<Status> userStatuses : allStatuses) {
-			if (userStatuses.size() > 0) {
-				long currentSinceId = Math.max(userStatuses.get(0).getId(), timeScanBegan.toId());
-				since_id = Math.min(since_id, currentSinceId);
-			}
-		}
-
-		if (since_id == Long.MAX_VALUE) {
-			since_id = -1;
-		}
-
-		return since_id;
-	}
-
-	private void getMedia(List<Status> statuses) {
 		for (Status status : statuses) {
 			for (ExtendedMediaEntity media : status.getExtendedMediaEntities()) {
 				// System.out.println(media.getMediaURL());
@@ -203,8 +221,12 @@ public class TwitterImageSync {
 				}
 
 				// format path
-				// todo: if (retweet) { create subdirectory for user }
-				String username = status.getUser().getScreenName();
+				// TODO if (retweet) { create subdirectory for user }
+				String username = status.getUser().getName(); // double check
+																// this is
+																// actually
+																// username
+				usernames.add(username);
 
 				DateFormat df = new SimpleDateFormat("yyMMdd-HHmmss");
 				Date d = status.getCreatedAt();
@@ -215,58 +237,49 @@ public class TwitterImageSync {
 				File file = new File(username + File.separator + filename);
 
 				if (!file.exists()) {
-					// get file
-					URL url;
 					try {
-						url = new URL(urlString);
+						URL url = new URL(urlString);
+						
+						byte[] response = new byte[0];
+
+						// get file
+						try (InputStream inputStream = new BufferedInputStream(url.openStream());
+								ByteArrayOutputStream outputStream = new ByteArrayOutputStream()) {
+							byte[] byteBuffer = new byte[1024];
+							int n = 0;
+							while (-1 != (n = inputStream.read(byteBuffer))) {
+								outputStream.write(byteBuffer, 0, n);
+							}
+							response = outputStream.toByteArray();
+						}
+						
+						file.getParentFile().mkdirs();
+
+						// save file
+						try (FileOutputStream fos = new FileOutputStream(file)) {
+							fos.write(response);
+						}
+
+						// write log
+						// http://stackoverflow.com/questions/1625234/how-to-append-text-to-an-existing-file-in-java
+						try (FileWriter fw = new FileWriter(username + File.separator + username + ".txt", true);
+								BufferedWriter bw = new BufferedWriter(fw);
+								PrintWriter pw = new PrintWriter(bw)) {
+							pw.println(filename + '_' + Long.toString(status.getId()));
+						}
 					} catch (MalformedURLException e1) {
 						// TODO Auto-generated catch block
 						e1.printStackTrace();
-					}
-					try {
-						InputStream in = new BufferedInputStream(url.openStream());
-						ByteArrayOutputStream out = new ByteArrayOutputStream();
-						byte[] buf = new byte[1024];
-						int n = 0;
-						while (-1 != (n = in.read(buf))) {
-							out.write(buf, 0, n);
-						}
-						out.close();
-						in.close();
-						byte[] response = out.toByteArray();
 					} catch (IOException e) {
 						// TODO Auto-generated catch block
 						e.printStackTrace();
-					}
-					
-					// save file
-					file.getParentFile().mkdirs();
-
-					try (FileOutputStream fos = new FileOutputStream(file)) {
-						fos.write(response);
-						fos.close();
-					} catch (IOException e) {
-						e.printStackTrace();
-					}
+					} 
 				}
 			}
 		}
 	}
 
-	/*
-	 * private boolean log() { FileOutputStream out = null;
-	 * 
-	 * try { out = new FileOutputStream("output.txt");
-	 * 
-	 * out.write(scanTime); out.write(newestImageTime); } catch (IOException e)
-	 * { e.printStackTrace(); } finally { if (out != null) { out.close(); } } }
-	 */
-
-	// long since_id = initFollowing();
-
-	// List<Status> newStatuses = getStatuses("", since_id, -1);
-
-	// todo retweet dupe
+	// TODO retweet dupe
 
 	public static void main(String[] args) {
 		TwitterImageSync tis = new TwitterImageSync();
@@ -278,26 +291,16 @@ public class TwitterImageSync {
 			System.out.println(te.getMessage());
 		}
 
-		/*
-		 * boolean init = false; // get this from log file
-		 * 
-		 * if (!init) { tis.initFollowing(); }
-		 */
-
-		// startup -> getFriends() compare to txt filenames
-		// if (!file.exists()) { getStatuses(); }
-		// else { getStatuses(); since_id = Math.max(scanTime - 1 day,
-		// newestImageTime); }
-
-		List<Status> statuses = tis.getStatuses(args[0], -1, -1);
-
-		tis.getMedia(statuses);
+		try {
+			tis.sync();
+		} catch (TwitterException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
 		/*
 		 * for (Status status : statuses) { System.out.println("@" +
 		 * status.getUser().getScreenName() + " - " + status.getText()); }
 		 */
-
 	}
-
 }
